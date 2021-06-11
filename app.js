4b36bc2f90d37058c7f0b948dafbe5c9d58f69c7
Thy Tran <70310080+ddthies42@users.noreply.github.com>
2021-06-10 20:45:30 -0700

    finalized

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // To be able to use the API everywhere
 app.use((request, response, next) => {
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Access-Control-Allow-Credentials", "true");
+  //response.setHeader("Access-Control-Allow-Origin", "*");
+  //response.setHeader("Access-Control-Allow-Credentials", "true");
   response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  //response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
