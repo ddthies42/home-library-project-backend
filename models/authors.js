e6bcf21183c6fc69c8425e57d4271a4374f3295d
Thy Tran <70310080+ddthies42@users.noreply.github.com>
2021-06-13 19:42:22 -0700

    change app.js to routes/books.js

--- models/authors.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-let Schema = mongoose.Schema;
-
-let AuthorSchema = new Schema({
-    name: String,
-    nationality: String
-});
-
-module.exports = mongoose.model('Author', AuthorSchema);
