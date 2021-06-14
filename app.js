a3b99d7d3eadcfbf77979909061f9209ea3f5f05
Thy Tran <70310080+ddthies42@users.noreply.github.com>
2021-06-13 23:39:51 -0700

    change app.js to routes/books.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var bookRouter = require('./routes/books');
-var db = require('./database');
+var indexRouter = require('./routers/index');
+var usersRouter = require('./routers/users');
+var bookRouter = require('./routers/books');
+var db = require('./db');
 
 var app = express();
 
